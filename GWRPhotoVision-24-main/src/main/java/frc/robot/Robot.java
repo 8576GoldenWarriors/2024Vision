9f534046a258641d7606b75c29247c456fd76d41
Chanakya <109757808+ChanakyaReddyGop@users.noreply.github.com>
2024-02-28 18:04:27 -0600

    add ledDisplay

--- GWRPhotoVision-24-main/src/main/java/frc/robot/Robot.java
+++ GWRPhotoVision-24-main/src/main/java/frc/robot/Robot.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.AprilTagStats;
+import frc.robot.commands.ledDisplay;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -46,6 +47,7 @@ public class Robot extends TimedRobot {
     CommandScheduler.getInstance().run();
 
     ats.getStats(); // gets the yaw, pitch, and XYZ coords of the bot
+    ledDisplay.shootingColor();
   }
 
   /** This function is called once each time the robot enters Disabled mode. */
